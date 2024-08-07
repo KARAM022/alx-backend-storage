79e0d8c7943f751c055250d0864eedece715310a
KARAM022 <my0fav0library@gmail.com>
2024-08-07 20:18:08 +0100

    alx

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,14 @@
+-- 4. Buy buy buy
+
+DELIMITER //
+
+CREATE TRIGGER after_insert_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END//
+
+DELIMITER ;
