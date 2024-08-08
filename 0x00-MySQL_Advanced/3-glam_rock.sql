828ba8128773a318d27156d2d2245ca1e772d6f1
KARAM022 <my0fav0library@gmail.com>
2024-08-08 13:20:27 +0100

    0x00

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,11 @@
 -- 3. Old school band
 
-SELECT band_name, IFNULL(split, 2020) - formed AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+SELECT
+    band_name,
+    IFNULL(split, 2020) - formed AS lifespan
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY
+    lifespan DESC;
