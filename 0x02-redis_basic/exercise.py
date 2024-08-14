bfb5de83385752d9a99748573c6eb740cdf9fbe1
KARAM022 <my0fav0library@gmail.com>
2024-08-14 15:21:10 +0100

    0x02

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,38 +1,103 @@
 #!/usr/bin/env python3
-""" Redis exercise """
-
-
+'''CMNT CMNT
+'''
+from functools import wraps
+from typing import Any, Callable, Union
 import redis
 import uuid
-from typing import Union
+
+
+def count_calls(method: Callable) -> Callable:
+    '''CMNT CMNT
+    '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        '''CMNT CMNT
+        '''
+
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    '''CMNT CMNT
+    '''
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        '''CMNT CMNT
+        '''
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return invoker
+
+
+def replay(fn: Callable) -> None:
+    '''CMNT CMNT
+    '''
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
 
 
 class Cache:
-    """ Cache class """
-    def __init__(self):
-        """ Initialize Cache class """
+    '''CMNT CMNT
+    '''
+
+    def __init__(self) -> None:
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Store data in Redis """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+    @call_history
+    @count_calls
+    def store(self, data:  Union[str, bytes, int, float]) -> str:
+        '''CMNT CMNT
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
 
-    def get(self, key: str, fn=None) -> Union[str, bytes, int, float]:
-        """ Get data from Redis """
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''CMNT CMNT
+        '''
         data = self._redis.get(key)
-        if data is None:
-            return None
-        if fn is not None:
-            return fn(data)
-        return data
-    
+        return fn(data) if fn is not None else data
+
     def get_str(self, key: str) -> str:
-        """ Get data from Redis as string """
-        return self.get(key, str)
-    
+        '''CMNT CMNT
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
     def get_int(self, key: str) -> int:
-        """ Get data from Redis as int """
-        return self.get(key, int)
+        '''CMNT CMNT
+        '''
+        return self.get(key, lambda x: int(x))
